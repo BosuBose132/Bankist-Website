2c4980d69137d2ad551058381f90e7a33f1420e4
Bosu <badebosubabu@gmail.com>
2026-04-08 11:11:38 -0400

    add Smooth scrolling affect when clicked on a btn

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
 const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
@@ -38,7 +41,7 @@ console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
 
-const header = document.querySelector('.header');
+//const header = document.querySelector('.header');
 const allSections = document.querySelectorAll('.section');
 console.log(allSections);
 
@@ -48,68 +51,83 @@ console.log(allButtons);
 
 console.log(document.getElementsByClassName('btn'));
 
+btnScrollTo.addEventListener('click', function (e) {
+  const s1cords = section1.getBoundingClientRect();
+  console.log(s1cords);
+
+  //Scrolling - Old school way
+  // window.scrollTo({
+  //   left: s1cords.left + window.pageXOffset,
+  //   top: s1cords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  //Latest way
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
 // Creating and inserting elements
 // .insertAdjacentHTML
 
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-message.innerHTML =
-  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// message.innerHTML =
+//   'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
 
-//header.prepend(message);
-header.append(message);
-// header.append(message.cloneNode(true));
+// //header.prepend(message);
+// header.append(message);
+// // header.append(message.cloneNode(true));
 
-//header.before(message);
-// header.after(message);
+// //header.before(message);
+// // header.after(message);
 
-// Deleting elements
-document
-  .querySelector('.btn--close-cookie')
-  .addEventListener('click', function () {
-    message.remove();
-  });
+// // Deleting elements
+// document
+//   .querySelector('.btn--close-cookie')
+//   .addEventListener('click', function () {
+//     message.remove();
+//   });
 
 // Styles
-message.style.backgroundColor = '#37383d';
-message.style.width = '120%';
-message.style.height = '100px';
-console.log(message.style.height);
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
+// message.style.height = '100px';
+// console.log(message.style.height);
 
-console.log(getComputedStyle(message).color);
-console.log(getComputedStyle(message).height);
+// console.log(getComputedStyle(message).color);
+// console.log(getComputedStyle(message).height);
 
-message.style.height =
-  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
 
-//document.documentElement.style.setProperty('--color-primary', 'orangered');
+// //document.documentElement.style.setProperty('--color-primary', 'orangered');
 
-// Attributes
-const logo = document.querySelector('.nav__logo');
-console.log(logo.alt);
-console.log(logo.className);
+// // Attributes
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.className);
 
-logo.alt = 'Beautiful minimalist logo';
-console.log(logo.alt);
+// logo.alt = 'Beautiful minimalist logo';
+// console.log(logo.alt);
 
-// Non-standard
-console.log(logo.designer);
-console.log(logo.getAttribute('designer'));
-logo.setAttribute('company', 'Bankist');
-console.log(logo.getAttribute('company'));
+// // Non-standard
+// console.log(logo.designer);
+// console.log(logo.getAttribute('designer'));
+// logo.setAttribute('company', 'Bankist');
+// console.log(logo.getAttribute('company'));
 
-const link = document.querySelector('.nav__link--btn');
-console.log(link.href);
-console.log(link.getAttribute('href'));
+// const link = document.querySelector('.nav__link--btn');
+// console.log(link.href);
+// console.log(link.getAttribute('href'));
 
-// Data attributes
-console.log(logo.dataset.versionNumber);
+// // Data attributes
+// console.log(logo.dataset.versionNumber);
 
-// Classes
-logo.classList.add('c', 'j');
-logo.classList.remove('c', 'j');
-logo.classList.toggle('c');
-console.log(logo.classList.contains('c'));
+// // Classes
+// logo.classList.add('c', 'j');
+// logo.classList.remove('c', 'j');
+// logo.classList.toggle('c');
+// console.log(logo.classList.contains('c'));
 
-// Don't use
-logo.className = 'jonas';
+// // Don't use
+// logo.className = 'jonas';
