b26f557f0e96986af84c00db5dd17acf4991329d
Bosu <badebosubabu@gmail.com>
2026-04-10 12:20:45 -0400

    add stick navigation

--- script.js
+++ script.js
@@ -111,6 +111,15 @@ const handleHover = function (e) {
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
 
+// Sticky Navigation
+const initialCoords = section1.getBoundingClientRect();
+window.addEventListener('scroll', function () {
+  console.log(window.scrollY);
+
+  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
+
 //Page navigation
 // document.querySelectorAll('.nav__link').forEach(function (el) {
 //   el.addEventListener('click', function (e) {
